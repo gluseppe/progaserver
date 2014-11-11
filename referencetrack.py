4ea15dce5b927056c2cf0552372f09b9b11a5726
clancia <carlo.lancia@dblue.it>
2014-11-11 15:18:07 +0100

    trackSetAnalyzer
    Calcola media e ds della distanza (DXT+, SNX-) dei punti di un trackSet
    da una referenceTrack

--- referencetrack.py
+++ referencetrack.py
@@ -3,6 +3,8 @@
 import numpy as np
 import progaconstants as pgc
 import math
+import progaconstants
+import pdb
 
 class Point3D(object):
         
@@ -123,58 +125,99 @@ class ReferenceTrack(object):
         	return [(t[0].getNumpyVector(), t[1].getNumpyVector()) for t in zip(self.line[:-1], self.line[1:])]
 
         def distanceFromGivenPoint(self, p):
-        	distfromLeg = np.inf
-        	minDistLeg = -1
-        	line = self.lineAsListOfLegs()
-        	point = p.getNumpyVector()
-        	for leg, index in zip(line, range(len(line))):
-        		t = np.dot(leg[1]-leg[0], point-leg[0])/np.dot(leg[1]-leg[0], leg[1]-leg[0])
-        		print "check:", t
-        		if t >= 0 and t <= 1:
-        			candidateDist = np.sqrt( np.dot(leg[0] + t*(leg[1]-leg[0]) - point, leg[0] + t*(leg[1]-leg[0]) - point ) )
-        			if candidateDist <= distfromLeg:
-        				distfromLeg = candidateDist
-        				minDistLeg = index
-        	return (distfromLeg, minDistLeg)
+            distfromLeg = np.inf
+            minDistLeg = -1
+            line = self.lineAsListOfLegs()
+            point = p.getNumpyVector()
+            leftOrRight = 0.
+            for leg, index in zip(line, range(len(line))):
+                t = np.dot(leg[1]-leg[0], point-leg[0])/np.dot(leg[1]-leg[0], leg[1]-leg[0])
+                if t >= 0 and t <= 1:
+                    candidateDist = np.sqrt( np.dot(leg[0] + t*(leg[1]-leg[0]) - point, leg[0] + t*(leg[1]-leg[0]) - point ) )
+                    u = p.getNumpyVector()-leg[0]
+                    v = leg[1]-leg[0]
+                    leftOrRight = int(np.sign( u[0]*v[1] - v[0]*u[1] )) # right is +1, left is -1
+                    if candidateDist <= distfromLeg:
+                        distfromLeg = candidateDist
+                        minDistLeg = index
+            return (distfromLeg, minDistLeg, leftOrRight)
 
 
 def trackSetAnalyzer(trackSet, objReferenceTrack):
-	"""
-	trackSet must be a dictionry of the form
-	{
-	'id_1': [point3D_1, point3D_2, ...],
-	'id_2': [point3D_1, point3D_2, ...],
-	.
-	.
-	.
-	'id_n': [point3D_1, point3D_2, ...]
-	}
-	"""
-	data = dict( zip( range( len(objReferenceTrack) ), [[] for i in range( len(objReferenceTrack) )] ) )
+    """
+    trackSet must be a dictionry of the form
+    {
+    'id_1': [point3D_1, point3D_2, ...],
+    'id_2': [point3D_1, point3D_2, ...],
+    .
+    .
+    .
+    'id_n': [point3D_1, point3D_2, ...]
+    }
+    """
+    data = dict( zip( ['leg'+str(i) for i in range( len(objReferenceTrack.line)-1 )], [[] for i in range( len(objReferenceTrack.line)-1 )] ) )
 
-	for track in trackSet.values():
-		for point in track:
-			d, l = objReferenceTrack.distanceFromGivenPoint(point)
-			data[l].append(d)
-	# Process the list in data[i] to compute the relevant statistic
+    for track in trackSet.values():
+        for point in track:
+            d, l, s = objReferenceTrack.distanceFromGivenPoint(point)
+            if l < -1: print l
+            try:
+                data['leg'+str(l)].append(s*d)
+            except KeyError:
+                continue
+    for k, L in data.items():
+        print k, ':: mean = ', np.mean(L), 'sd = ', np.std(L)
 
+
+def loadTrack(trackfilename, track_id, flight_start):
+    #trackflilename does not include the path, it's only name and extesion
+    #track = open("./Flight1.txt", "r")
+    path = []
+    cont = 0
+
+    with open(trackfilename, "r") as trackfile:
+        for line in trackfile:
+            
+            if (not (line.startswith('#') or line.startswith('/'))) and len(line)>30:
+                parts = line.split()
+                timestamp = parts[0]
+                lat = float(parts[1])
+                lon = float(parts[2])
+                altitude = float(parts[3])*progaconstants.FOOT2MT
+                heading = float(parts[6])
+                v_x = float(parts[8])*progaconstants.FOOT2MT
+                v_y = float(parts[10])*progaconstants.FOOT2MT
+                v_z = float(parts[9])*progaconstants.FOOT2MT
+                pitch = float(parts[4])
+                bank = float(parts[5])
+
+                p = Point3D(lon, lat, altitude)
+
+                path.append(p)
+
+            cont += 1
+            
+    return path
 
 
 if __name__ == '__main__':
         p1 = Point3D(2.0664806, 48.8136806, 113.0808)
+        print 'P1', p1
         p2 = Point3D(1.9450000, 48.9983333,  113.0808)
+        print 'P2', p2
         p3 = Point3D(1.1761917, 49.3852111,  156.0576)
+        print 'P3', p3
         p4 = Point3D(0.5666667, 49.1027778,  168.8592)
+        print 'P4', p4
         L = [p1, p2, p3 , p4]
-        track = ReferenceTrack(L)
-
-
-
-
-
-
-
-
-
-
-
+        refTrack = ReferenceTrack(L)
+        ppp = Point3D(2.017732, 48.984049, 113.)
+        print refTrack.distanceFromGivenPoint(ppp)
+        pathToFlownTraces = '/Users/clancia/Google Drive/ProGA/Flight Simulator Tracks/'
+        flights = ['Flight3.txt', 'Flight5.txt']
+        flightFiles = [a + b for (a,b) in zip([pathToFlownTraces]*len(flights), flights)]
+        flightIDs = ['Hello_World_' + str(i+1) for i in range(len(flights))]
+        flightStarts = [0]*len(flights) # fix
+        flightObjects = [loadTrack(a,b,c) for (a,b,c) in zip(flightFiles, flightIDs, flightStarts) ]
+        trackSet = dict( zip(flightIDs, flightObjects) )
+        trackSetAnalyzer(trackSet, refTrack)
