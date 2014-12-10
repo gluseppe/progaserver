7c29e2648b93521f34ec8f63b69c183a6b5c4d34
Carlo Lancia <clancia@MacBook-Pro-di-Carlo.local>
2014-12-10 18:42:32 +0100

    History Analytics 2nd version
    Adesso loadTrack lavora con una lista di liste di numpy 3D

--- historyanalytics.py
+++ historyanalytics.py
@@ -3,13 +3,14 @@ import os, os.path
 from track import Track
 import glob
 import progaconstants
+import numpy as np
 
 
 
 class HistoryAnalitics(object):
-
-	
-
+	"""
+	I will help Timothy, Dumbo's helper
+	"""
 	def __init__(self, query_name):
 		self.root_folder = "./db/history"
 		self.query_name = query_name
@@ -20,48 +21,41 @@ class HistoryAnalitics(object):
 
 
 	def loadAll(self):
-		for name,i in zip(self.files_iterable,range(len(self.files_iterable))):
-			self.track_list.append(self.loadTrack(name,i))
+		"""
+		Creates a list of tracks
+		Each tracks is a list of 3D numpy vectors (LON, LAT, ALT)
+		"""
+		for name in self.files_iterable:
+			self.track_list.append(self.loadTrack(name))
 
 
+	def loadTrack(self, trackfilename):
+		"""
+		Reads flight data from trackfilename and returns a list of 3D numpy vectors (LON, LAT, ALT)
+		"""
+		track = []
+		with open(trackfilename, "r") as trackfile:
+			for line in trackfile:
+				if (not (line.startswith('#') or line.startswith('/'))) and len(line)>30:
+					parts = line.split()
+					lat = float(parts[1])
+					lon = float(parts[2])
+					altitude = float(parts[3])*progaconstants.FOOT2MT
+					track.append(np.array([lon, lat, altitude], dtype=np.float64))
+		return track
 
-
-
-
-	def loadTrack(self, trackfilename, track_id):
-			#trackflilename does not include the path, it's only name and extesion
-			#track = open("./Flight1.txt", "r")
-			track = Track(track_id)
-			cont = 0
-			with open(trackfilename, "r") as trackfile:
-				for line in trackfile:
-					
-					if (not (line.startswith('#') or line.startswith('/'))) and len(line)>30:
-						parts = line.split()
-						timestamp = parts[0]
-						lat = float(parts[1])
-						lon = float(parts[2])
-						altitude = float(parts[3])*progaconstants.FOOT2MT
-						heading = float(parts[6])
-						v_x = float(parts[8])*progaconstants.FOOT2MT
-						v_y = float(parts[10])*progaconstants.FOOT2MT
-						v_z = float(parts[9])*progaconstants.FOOT2MT
-						pitch = float(parts[4])
-						bank = float(parts[5])
-	
-						track.addStep(timestamp, lat, lon, altitude, v_x, v_y, v_z, heading, pitch, bank)
-	
-					cont += 1
-					
-			track.setStart(0)
-			return track
-
-
-if __name__ == '__main__':
+def debug():
+	"""
+	Just for testing purposes
+	"""
 	ha = HistoryAnalitics("query1")
 	tl = ha.track_list
 	print len(tl)
-	print tl[0].path[:3]
+	print tl[0][:3]
+
+
+if __name__ == '__main__':
+	debug()
 	
 
 
