d143231d16415df7c94b4dfe32e0fdc821c44721
Carlo Lancia <clancia@MacBook-Pro-di-Carlo.local>
2015-01-08 12:04:43 +0100

    History Analytics 4th version
    Added method expungeAroundDepArrAirfields() to exclude the airfield
    circuits from the computation of KDE
    The points are excluded by distance on Ellipsoidal Earth projected to a
    plane, see
    http://en.wikipedia.org/wiki/Geographical_distance#Ellipsoidal_Earth_pro
    jected_to_a_plane
    Cutoff distance is currently set to 10 km.

--- historyanalytics.py
+++ historyanalytics.py
@@ -5,8 +5,9 @@ import glob
 import progaconstants
 import numpy as np
 from scipy import stats
+from math import cos, sqrt
 
-
+DEFAULT_RADIUS = 10 # distance in Km
 
 class HistoryAnalitics(object):
 	"""
@@ -30,7 +31,7 @@ class HistoryAnalitics(object):
 			self.track_list.append(self.loadTrack(name))
 
 
-	def loadTrack(self, trackfilename):
+	def loadTrack(self, trackfilename, expunge=True, expRadius=DEFAULT_RADIUS):
 		"""
 		Reads flight data from trackfilename and returns a list of 3D numpy vectors (LON, LAT, ALT)
 		"""
@@ -43,7 +44,48 @@ class HistoryAnalitics(object):
 					lon = float(parts[2])
 					altitude = float(parts[3])*progaconstants.FOOT2MT
 					track.append(np.array([lon, lat, altitude], dtype=np.float64))
+		if expunge:
+			track = self.expungeAroundDepArrAirfields(track, expRadius)
 		return track
+
+	def expungeAroundDepArrAirfields(self, track, expRadius=DEFAULT_RADIUS):
+		"""
+		Whanasget?
+		"""
+		dep_lon = track[0][0]
+		dep_lat = track[0][1]
+		arr_lon = track[-1][0]
+		arr_lat = track[-1][1]
+		while True:
+			lon = track[-1][0]
+			lat = track[-1][1]
+			meanlat = .5 * (arr_lat + lat)
+			difflat = arr_lat - lat
+			difflon = arr_lon - lon
+			K1 = 111.13209 - .56605 * cos(2*meanlat) + .0012 * cos(4*meanlat)
+			K2 = 111.41513 * cos(meanlat) - .09455 * cos(3*meanlat) + .00012 * cos(5*meanlat)
+			distance = sqrt((K1*difflat)**2 + (K2*difflon)**2)
+			if distance < expRadius:
+				track.pop()
+			else:
+				break
+		track.reverse()
+		while True:
+			lon = track[-1][0]
+			lat = track[-1][1]
+			meanlat = .5 * (dep_lat + lat)
+			difflat = dep_lat - lat
+			difflon = dep_lon - lon
+			K1 = 111.13209 - .56605 * cos(2*meanlat) + .0012 * cos(4*meanlat)
+			K2 = 111.41513 * cos(meanlat) - .09455 * cos(3*meanlat) + .00012 * cos(5*meanlat)
+			distance = sqrt((K1*difflat)**2 + (K2*difflon)**2)
+			if distance < expRadius:
+				track.pop()
+			else:
+				break
+		return track
+
+
 
 	def summary(self):
 		"""
